55b93f14f348174190ee1e13154ea87889267df5
David R. Poindexter III <drpoindexter@gmail.com>
2016-03-26 09:17:18 -0400

    Store and actions setup

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import AppActions from '../actions/app-actions'
+import AppActions from '../actions/app-actions';
 
 export default class App extends React.Component {
-  render(){
-    return(
-      <div>
-        <h1
+    render() {
+        return <h1
         onClick={AppActions.addItem.bind(null, 'this is the item')}
-        >Hello!</h1>
-        <p>How are you?</p>
-      </div>
-    )
-  }
+        >Flux</h1>;
+    }
 }
